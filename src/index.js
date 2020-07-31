639342923f9e533abd1763b6c47164ab41c40c89
chakapega <chakapega@gmail.com>
2020-07-31 15:45:23 +0300

    feat: completed lesson 55

--- src/index.js
+++ src/index.js
@@ -1,47 +1,6 @@
-class SwapiService {
-  _apiBase = 'https://swapi.dev/api';
+import React from 'react';
+import ReactDom from 'react-dom';
 
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
+import App from './components/app/App';
 
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, received ${res.status}`);
-    }
-
-    return await res.json();
-  }
-
-  async getAllPeople() {
-    const res = await this.getResource('/people');
-
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResource(`/people/${id}`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResource('/planets');
-
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}`);
-  }
-
-  async getAllStarships() {
-    const res = await this.getResource('/starships');
-
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getResource(`/starships/${id}`);
-  }
-}
-
-const swapi = new SwapiService();
-
-swapi.getAllPlanets().then(data => console.log(data));
+ReactDom.render(<App />, document.querySelector('#root'));
