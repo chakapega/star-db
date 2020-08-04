9261278f1c0710faa46e8e8426ffc18e52bdd22f
chakapega <chakapega@gmail.com>
2020-08-04 19:55:09 +0300

    feat: completed lesson 70

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,37 +2,17 @@ import React, { Component } from 'react';
 
 import Header from '../header/Header';
 import RandomPlanet from '../randomPlanet/RandomPlanet';
-import ItemList from '../itemList/ItemList';
-import PersonDetails from '../personDetails/PersonDetails';
+import PeoplePage from '../peoplePage/PeoplePage';
 
 import './App.css';
 
 class App extends Component {
-  state = {
-    selectedPerson: null,
-  };
-
-  onPersonSelected = id => {
-    this.setState({
-      selectedPerson: id,
-    });
-  };
-
   render() {
-    const { selectedPerson } = this.state;
-
     return (
       <div>
         <Header />
         <RandomPlanet />
-        <div className="mb2 d-flex">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage />
       </div>
     );
   }
---
+++ src/components/peoplePage/PeoplePage.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import ItemList from '../itemList/ItemList';
+import PersonDetails from '../personDetails/PersonDetails';
+
+import './PeoplePage.css';
+
+export default class PeoplePage extends Component {
+  state = {
+    selectedPerson: null,
+  };
+
+  onPersonSelected = selectedPerson => {
+    this.setState({ selectedPerson });
+  };
+
+  render() {
+    const { selectedPerson } = this.state;
+
+    return (
+      <div className="d-flex mb2">
+        <div className="col-md-6">
+          <ItemList onItemSelected={this.onPersonSelected} />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails personId={selectedPerson} />
+        </div>
+      </div>
+    );
+  }
+}
